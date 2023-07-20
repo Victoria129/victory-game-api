36ea3dd9bf9ed8a32a1cc3ba10a84556662ae957
Victoria Tumwebaze <victoria.tumwebaze@outlook.com>
2023-07-20 12:41:26 +0300

    Update repository.js

--- repository.js
+++ repository.js
@@ -1,3 +1,5 @@
+const sqlite3 = require('sqlite3');
+
 class Repository {
   delete(id){
     console.log(`Delete ${id}`);
@@ -9,8 +11,27 @@ class Repository {
 
   create(item){
     console.log(item);
+    const db = new sqlite3.Database(
+      "mydatabase.db",
+      (err) => {
+        if (err){
+          console.log(err.message);
+          return -1;
+        }
+        console.log('connection is open');
+        db.run(`INSERT INTO item (name, description) VALUES("${item.name}", "${item.description}")`, (err) => {
+          if (err){
+            console.log(err.message);
+            return -2;
+          }
+          return this.lastID;
+        });
+      }
+    );
     return 0;
   }
 }
+
+
 
 module.exports = Repository;
